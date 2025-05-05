efeb805fc7313ddc79a7bfea4903372daa1f3882
Ethan Grané G <90970199+EthanGrane@users.noreply.github.com>
2025-05-05 16:43:08 +0200

    Project configured (not finished)
    El app.vue ahora muestra el routerView y se han creado las carpetas Layout y View en el proeycto

---
+++ tw-ts-vue/src/Layouts/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+    <nav>
+        <router-link to="/">Inicio</router-link>
+        <router-link to="/dashboard">Dashboard</router-link>
+    </nav>
+
+</template>
--- tw-ts-vue/src/routes/index.ts
+++ tw-ts-vue/src/routes/index.ts
@@ -1,12 +1,17 @@
 import { createMemoryHistory, createRouter } from "vue-router"
 import HelloWorld from "../components/HelloWorld.vue"
-
+import Dashboard from "../components/Dashboard.vue"
 
 const routes = [
     {
         path: '/',
         name: 'home',
         component: HelloWorld
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: Dashboard
     }
 ]
 
