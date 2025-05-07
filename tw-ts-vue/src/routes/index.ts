2e87759fe78102c2ad9abda53e3c38e25391e238
Óliver Ros <105983648+oliverdev75@users.noreply.github.com>
2025-05-07 20:23:33 +0200

    commit

--- tw-ts-vue/src/routes/index.ts
+++ tw-ts-vue/src/routes/index.ts
@@ -1,22 +1,8 @@
-import { createMemoryHistory, createRouter } from "vue-router"
-import HelloWorld from "../components/HelloWorld.vue"
-import Dashboard from "../components/Dashboard.vue"
-
-const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: HelloWorld
-    },
-    {
-        path: '/dashboard',
-        name: 'dashboard',
-        component: Dashboard
-    }
-]
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from './routes'
 
 const router = createRouter({
-    history: createMemoryHistory(),
+    history: createWebHistory(),
     routes
 })
 
