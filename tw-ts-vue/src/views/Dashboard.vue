757de20a59507d519b2983d95b306eaab5136724
Ethan Grané G <90970199+EthanGrane@users.noreply.github.com>
2025-05-14 20:53:51 +0200

    Dashboard view

--- tw-ts-vue/src/views/Dashboard.vue
+++ tw-ts-vue/src/views/Dashboard.vue
@@ -1,21 +1,39 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import useApi from '../composables/api';
 
-import {onMounted} from "vue";
-import useApi from "../composables/api.ts";
+const api = useApi();
 
 onMounted(async () => {
-    const api = useApi();
-    
+   
 })
 
+const login = async () => {
+  try {
+    const response = await api.post('api/login', {
+      email: 'test@demo.com'
+    });
+    console.log('Login exitoso:', response.data);
+  } catch (error) {
+    console.error('Error al iniciar sesión:', error);
+  }
+};
 </script>
 
 <template>
-    <div class="mainDashboardGrid">
-        <div class="grid-item" style=" grid-area: 1 / 2 / 2 / 3;"> </div>
-        <div class="grid-item" style=" grid-area: 2 / 2 / 6 / 3;"> </div>
-        <div class="grid-item" style=" grid-area: 1 / 1 / 4 / 2;"> </div>
-        <div class="grid-item" style=" grid-area: 4 / 1 / 6 / 2;"> </div>
+    <div class="background">
+        <div class="mainDashboardGrid">
+
+            <div class="grid-item" style=" grid-area: 1 / 2 / 2 / 3; background: #9EECA0;"> 
+                <button @click="login">
+                    Login!
+                </button>
+            </div>
+
+            <div class="grid-item" style=" grid-area: 2 / 2 / 6 / 3; background: #9D8DE0;"> </div>
+            <div class="grid-item" style=" grid-area: 1 / 1 / 4 / 2; background: #9D8DE0;"> </div>
+            <div class="grid-item" style=" grid-area: 4 / 1 / 6 / 2; background: #9EECA0;"> </div>
+        </div>
     </div>
 </template>
 
@@ -29,15 +47,17 @@ onMounted(async () => {
 
     gap: 3rem;
 
-    width: 85%;
-    height: 85vh;
     margin: auto;
+    margin-top: 16px;
+    width: 90%;
+    height: 90vh;
 }
 
 .grid-item {
     width: 100%;
     height: 100%;
+
     border-radius: 1rem;
-    background-color: aqua;
+    box-shadow: 0 0 8px rgb(240, 240, 240);
 }
 </style>
