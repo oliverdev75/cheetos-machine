3984d830f315254bafc09f0c0a3dbdbb716dc4e9
nilbalaguer <nilbalaguerfp@ibf.cat>
2025-05-16 16:14:01 +0200

    Crud de user y product

---
+++ api/app/routes/order.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+from flask import jsonify, request
+from app import api, db
+from app.database.models import Order
+
--- api/app/routes/product.py
+++ api/app/routes/product.py
@@ -1,8 +1,10 @@
-from flask import jsonify
-from app import api
+from datetime import datetime
+from flask import jsonify, request
+from app import api, db
 from app.database.models import Product
 from app.constants import API_PREFIX
 
+#Personalizadas
 @api.route("/buy/<id>")
 def buy_product(id = None):
     if not id:
@@ -11,3 +13,46 @@ def buy_product(id = None):
     if not products:
         return jsonify("no product with that id found"), 404
     return products[0].to_dict()
+
+#Crud
+@api.route('/product', methods=['GET'])
+def get_products():
+    return jsonify([p.to_dict() for p in Product.query.all()])
+
+
+@api.route('/product/<int:id>', methods=['GET'])
+def get_product(id):
+    return jsonify(Product.query.get_or_404(id).to_dict())
+
+
+@api.route('/product', methods=['POST'])
+def create_product():
+    data = request.get_json()
+    product = Product(
+        name=data['name'],
+        price=data['price'],
+        image=data['image']
+    )
+    db.session.add(product)
+    db.session.commit()
+    return jsonify(product.to_dict()), 201
+
+
+@api.route('/product/<int:id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get_or_404(id)
+    data = request.get_json()
+    product.name = data.get('name', product.name)
+    product.price = data.get('price', product.price)
+    product.image = data.get('image', product.image)
+    product.updated_at = datetime.now()
+    db.session.commit()
+    return jsonify(product.to_dict())
+
+
+@api.route('/product/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get_or_404(id)
+    db.session.delete(product)
+    db.session.commit()
+    return jsonify({ 'message': 'Product deleted' })
