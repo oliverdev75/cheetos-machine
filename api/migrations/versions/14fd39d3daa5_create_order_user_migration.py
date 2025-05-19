e2cf69483bba81dc508d478882a1b6f88b682ee2
Ethan Grané G <90970199+EthanGrane@users.noreply.github.com>
2025-05-19 16:41:38 +0200

    order_user_migration

--- api/migrations/versions/14fd39d3daa5_create_order_user_migration.py
+++ api/migrations/versions/14fd39d3daa5_create_order_user_migration.py
@@ -1,8 +1,8 @@
-"""create user_role table
+"""create_order_user_migration
 
-Revision ID: 16eca1cbe69e
-Revises: cdc7d4ab63a9
-Create Date: 2025-05-13 13:35:28.714560
+Revision ID: 14fd39d3daa5
+Revises: 866eee13f60d
+Create Date: 2025-05-19 16:35:54.674910
 
 """
 from alembic import op
@@ -10,24 +10,24 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '16eca1cbe69e'
-down_revision = 'cdc7d4ab63a9'
+revision = '14fd39d3daa5'
+down_revision = '866eee13f60d'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.create_table('user_order',
+    op.create_table('user_role',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('user_id', sa.Integer(), nullable=False),
-    sa.Column('role_id', sa.Integer(), nullable=False),
+    sa.Column('order_id', sa.Integer(), nullable=False),
     sa.Column('created_at', sa.DateTime(), nullable=False),
     sa.Column('updated_at', sa.DateTime(), nullable=True),
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
-    sa.ForeignKeyConstraint(['role_id'], ['roles.id'], ),
+    sa.ForeignKeyConstraint(['order_id'], ['orders.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
 
 
 def downgrade():
-    op.drop_table('user_order')
+    pass
